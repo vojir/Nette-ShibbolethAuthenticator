a2e09f4cd60fe04155d5675bed76bd721cfb6912
Stanislav Vojíř <stanislav.vojir@gmail.com>
2019-10-24 02:03:57 +0200

    DI extension modified

--- src/DI/ShibbolethAuthExtension.php
+++ src/DI/ShibbolethAuthExtension.php
@@ -2,8 +2,8 @@
 
 namespace Vojir\ShibbolethAuthenticator\DI;
 
-use Nette;
 use Vojir\ShibbolethAuthenticator\ShibbolethAuthenticator;
+use Nette\Utils\Validators;
 
 /**
  * Class ShibbolethAuthExtension
@@ -11,7 +11,7 @@ use Vojir\ShibbolethAuthenticator\ShibbolethAuthenticator;
  * @author Stanislav Vojíř
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  */
-class ShibbolethAuthExtension extends Nette\DI\CompilerExtension{
+class ShibbolethAuthExtension extends \Nette\DI\CompilerExtension{
 
   private $defaults = [
     'URLs'=>[
@@ -30,12 +30,12 @@ class ShibbolethAuthExtension extends Nette\DI\CompilerExtension{
 
   public function loadConfiguration(){
     $config=$this->validateConfig($this->defaults);
-    Nette\Utils\Validators::assert($config['URLs']['sessionInitiator'], 'url', 'URLs.sessionInitiator');
-    Nette\Utils\Validators::assert($config['URLs']['sessionLogout'], 'url', 'URLs.sessionLogout');
-    Nette\Utils\Validators::assert($config['variables']['username'], 'string', 'variables.username');
-    Nette\Utils\Validators::assert($config['variables']['email'], 'string', 'variables.email');
-    Nette\Utils\Validators::assert($config['variables']['displayName'], 'string', 'variables.displayName');
-    Nette\Utils\Validators::assert($config['variables']['shibSessionId'], 'string', 'variables.shibSessionId');
+    Validators::assert($config['URLs']['sessionInitiator'], 'url', 'URLs.sessionInitiator');
+    Validators::assert($config['URLs']['sessionLogout'], 'url', 'URLs.sessionLogout');
+    Validators::assert($config['variables']['username'], 'string', 'variables.username');
+    Validators::assert($config['variables']['email'], 'string', 'variables.email');
+    Validators::assert($config['variables']['displayName'], 'string', 'variables.displayName');
+    Validators::assert($config['variables']['shibSessionId'], 'string', 'variables.shibSessionId');
 
     $container = $this->getContainerBuilder();
     $container->addDefinition($this->prefix('shibboleth'))
@@ -43,8 +43,8 @@ class ShibbolethAuthExtension extends Nette\DI\CompilerExtension{
       ->addSetup('$config',[$config]);
   }
 
-  public static function register(Nette\Configurator $configurator){
-    $configurator->onCompile[] = function ($config, Nette\DI\Compiler $compiler) {
+  public static function register(\Nette\Configurator $configurator){
+    $configurator->onCompile[] = function ($config, \Nette\DI\Compiler $compiler) {
       $compiler->addExtension('shibboleth', new ShibbolethAuthExtension());
     };
   }
